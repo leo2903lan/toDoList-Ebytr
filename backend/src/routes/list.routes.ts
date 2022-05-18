fd8bf22b65af339e323c24f228917c324af1df44
Leandro Nogueira <leandro2903.lan@gmail.com>
2022-05-18 18:24:04 -0300

    CRUD para as rotas user e list, com utilização das funções e controle do banco com o PrismaORM, atravez das migrations

--- backend/src/routes/list.routes.ts
+++ backend/src/routes/list.routes.ts
@@ -1,24 +1,57 @@
+import { PrismaClient } from '@prisma/client'
 import { Router, Request, Response } from 'express';
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Lista completa de tarefa')
-});
-router.get('/:id', (req: Request, res: Response) => {
-  res.send('Buscar por id de tarefa!')
-});
-
-router.post('/', function (req: Request, res: Response) {
-  res.send('Criando uma tarefa');
+router.get('/', async (req: Request, res: Response) => {
+  const tasks = await prisma.list.findMany();
+  res.status(200).json(tasks);
 });
 
-router.put('/', function (req: Request, res: Response) {
-  res.send('Editando uma tarefa');
+router.get('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const task = await prisma.list.findUnique({ where: { id: Number(id)}});
+  res.status(200).json(task);
 });
 
-router.delete('/', function (req: Request, res: Response) {
-  res.send('Deletando tarefa');
+router.post('/',   async (req: Request, res: Response) => {
+  const { title, content, pubcompleted } = req.body;
+
+  await prisma.list.create({
+    data: {
+      title,
+      content,
+      pubcompleted,
+    }
+  })
+  res.status(201).json({ message: 'Task Created!' });
+});
+
+router.put('/', async  (req: Request, res: Response) => {
+  const { title, content, pubcompleted } = req.body;
+  const { id } = req.params;
+
+  await prisma.list.update({
+    where: { id: Number(id) },
+    data: {
+      title,
+      content,
+      pubcompleted,
+    }
+  })
+  res.status(200).json({ message: 'Task updated!' });
+});
+
+router.delete('/', async (req: Request, res: Response) => {
+  await prisma.list.deleteMany();
+  res.status(200).json({ message: 'All task Deleted ' });
+});
+router.delete('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  await prisma.list.delete({ where: { id:Number(id) }})
+  res.status(200).json({ message: 'Task Deleted' });
 });
 
 export default router;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,19 +1,30 @@
+import { PrismaClient } from '@prisma/client'
 import { Router, Request, Response } from 'express';
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Lista usuários cadastrados')
+router.get('/', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
 });
 
-router.get('/:username', (req: Request, res: Response) => {
-  const{ username } = req.params;
-  
-  res.send(`Buscar por usuário específico ${ username } !`)
+router.get('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({ where: { id: Number(id)}});
+  res.status(200).json(user);
 });
 
-router.post('/', function (req: Request, res: Response) {
-  res.send('Criando usuário');
+router.post('/',   async (req: Request, res: Response) => {
+  const { email, password, username, } = req.body;
+
+  await prisma.user.create({
+    data: {
+      email,
+      password,
+      username,
+  }})
+  res.status(201).json({ message: 'User Created!' });
 });
 
 export default router;
