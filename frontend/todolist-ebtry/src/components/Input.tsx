2ab041b47912b721b3b09d66a870f39bfc71eb91
Leandro Nogueira <leandro2903.lan@gmail.com>
2022-05-19 11:45:49 -0300

    deixadno os componetes de input text e select com estado controlado para garantir a integridade do conteúdo digitado e selecionado, deixando a estrutura das função que farão contato com a API para dar vida aos botão e trocar os dados com o banco.

--- frontend/todolist-ebtry/src/components/Input.tsx
+++ frontend/todolist-ebtry/src/components/Input.tsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Input() {
+  const [taskValue , setTaskValue] = useState('');
+  const [selectValue , setSelectValue] = useState('pendente');
+
+  const addTask = () => { console.log('requisição na API para adicionar a TASK') }
+  const deleteTask = () => { console.log('requisição na API para detelar a TASK selecionada') }
+  const updateTask = () => { console.log('requisição na API para atualizar a TASK selecionada') }
+
   return (
     <form>
-      <input type="checkbox" />
-    <label>
-      Task:
-      <input type="text" name="name" />
-    </label>
-    <select>
-      <option selected value="pendente">Pendente</option>
-      <option value="andamento">Andamento</option>
-      <option value="pronto">Pronto</option>
-    </select>
-    <button type="submit" value="Add" >Add Task</button>
-    <button type="submit" value="DELETE" >DELETE</button>
-    <button type="submit" value="Atualizar" >Atualizar</button>
-  </form>
+      <label>
+        Task:
+        <input type="text" name="task" value={ taskValue } onChange={ e => setTaskValue(e.target.value) } />
+      </label>
+      
+      <select value={ selectValue } onChange={ e => setSelectValue(e.target.value) }>
+        <option selected value="pendente">Pendente</option>
+        <option value="andamento">Andamento</option>
+        <option value="pronto">Pronto</option>
+      </select>
+
+      <button type="button" value="Add" onClick={addTask}>Add Task</button>
+      <button type="button" value="DELETE" onClick={deleteTask}>DELETE</button>
+      <button type="button" value="Atualizar" onClick={updateTask}>Atualizar</button>
+    </form>
   )
 }
 
