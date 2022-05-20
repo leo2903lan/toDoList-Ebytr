c64f7623454c2323dbf950a8faf4cf33daabc7f9
Leandro Nogueira <leandro2903.lan@gmail.com>
2022-05-20 11:10:13 -0300

    header com o título no app

--- frontend/todolist-ebtry/src/App.tsx
+++ frontend/todolist-ebtry/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import Input from './components/Input';
 
 function App() {
   return (
     <>
+      <Header />
       <Input />
     </>
   );
