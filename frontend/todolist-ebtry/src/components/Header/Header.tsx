5f0f14b9cf1dfb7cbd12cf1a36d25ba866057a47
Leandro Nogueira <leandro2903.lan@gmail.com>
2022-05-20 11:41:14 -0300

    estilizadno o componente header com css

---
+++ frontend/todolist-ebtry/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  './header.css'
+
+function Header() {
+  return (
+    <div className='box-title'>
+      <h1 className='tittle-collor'>To Do List EbTry</h1>
+    </div>
+  )
+}
+
+export default Header
